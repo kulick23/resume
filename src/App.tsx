b6c947835ce776ba79c37a7bed1e41adca50c47d
Danila Kulinkovich <danek200326@gmail.com>
2025-08-31 17:30:46 +0300

    update to vite, update versions

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState, useMemo, useEffect } from 'react';
-import './App.css';
+import './App.scss';
 import About from "./Componets/About/About";
 import Skills from "./Componets/Skills/Skills";
 import Project from "./Componets/Projects/Project";
