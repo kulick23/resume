a74b4d0ffab3bcc6950b6b87e0912473079e4390
Danila Kulinkovich <danek200326@gmail.com>
2025-09-05 22:29:54 +0300

    update

--- src/Componets/Projects/Project.tsx
+++ src/Componets/Projects/Project.tsx
@@ -1,28 +1,13 @@
 import type React from 'react';
-import { useState, useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
 import { projectsStore } from '../../Stores';
 import Laptop from '../Laptop';
 import { useTranslation } from 'react-i18next';
-import { Element } from 'react-scroll';
 import './Project.scss';
-import { useChunkProjects } from '../../Hooks';
-import type { Project as ProjectType } from '../../types';
-
-const getChunkSize = () => (window.innerWidth >= 1024 ? 3 : window.innerWidth >= 768 ? 2 : 1);
 
 export const Project: React.FC = observer(() => {
-  const { filteredProjects, selectedCategory, setSelectedCategory } = projectsStore; // используем отфильтрованные проекты
+  const { filteredProjects, selectedCategory, setSelectedCategory } = projectsStore;
   const { t } = useTranslation();
-  const [chunkSize, setChunkSize] = useState(getChunkSize());
-
-  useEffect(() => {
-    const handleResize = () => setChunkSize(getChunkSize());
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
-
-  const chunkedProjects = useChunkProjects<ProjectType>(filteredProjects, chunkSize); // используем отфильтрованные проекты
 
   const handleFilterChange = (category: 'all' | 'commercial' | 'pet') => {
     setSelectedCategory(category);
@@ -53,20 +38,14 @@ export const Project: React.FC = observer(() => {
         </button>
       </div>
 
-      {chunkedProjects.map((row, rowIndex) => (
-        <Element
-          key={rowIndex}
-          name={`project-row-${rowIndex + 1}`}
-          className="project__row"
-          data-aos="fade-up"
-        >
-          {row.map((project) => (
-            <div key={project.title} className="project__item">
-              <Laptop project={project} />
-            </div>
-          ))}
-        </Element>
-      ))}
+      {/* Один блок для всех проектов */}
+      <div className="project__container" data-aos="fade-up">
+        {filteredProjects.map((project) => (
+          <div key={project.title} className="project__item">
+            <Laptop project={project} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 });
